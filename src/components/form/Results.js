e954573eccb9d9ba71f5829da2abf41782033cf2
Amit Ashdot <amitashdot@gmail.com>
2022-02-27 16:22:44 +0200

    no vendor list

--- src/components/form/Results.js
+++ src/components/form/Results.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 // import ChoseType from './ChoseType'
 // import RevRange from './RevRange'
@@ -6,13 +6,16 @@ import { Link } from 'react-router-dom'
 
 import { useDispatch, useSelector } from 'react-redux';
 // import Industry from './Industry';
-import { closeResults, findAverage } from '../../store/form/action';
+import { closeResults, findAverage, toggleMailForm } from '../../store/form/action';
+import { faCheckCircle, faPaperPlane, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Results = ({ industryeArr }) => {
     const dispatch = useDispatch();
 
     const formState = useSelector(state => state.formReducer);
     const dataState = useSelector(state => state.dataReducer);
+
 
     useEffect(() => {
         dispatch(findAverage(industryeArr));
@@ -23,8 +26,21 @@ const Results = ({ industryeArr }) => {
         dispatch(closeResults())
     }
 
+
+    const handleToggle = (e) => {
+        e.preventDefault();
+        dispatch(toggleMailForm(!formState.mailForm))
+    }
     const createRefferalLinks = formState.vendor === '1' ?
-        formState.avgArr.map(element => { return <div><p>Name:{element.name}</p><p>Avg:{element.avg}</p></div> })
+        formState.avgArr.map(element =>
+        (<div>
+            <p>Name:{element.name}</p>
+            <p>Avg:{element.avg}</p>
+            <div className='referral-logo'>
+                <img defer src={element.image} alt={element.name} width="12" height="50" />
+            </div>
+        </div>)
+        )
         :
         dataState.referralLinks.map((referral, key) => {
             return (<Link to={referral.link} className="btn referral" key={key}> {referral.company}</Link>)
@@ -32,8 +48,19 @@ const Results = ({ industryeArr }) => {
 
     return (
         <div className='results'>
-            <button onClick={handleClose}>CLOSE</button>
+
+            <FontAwesomeIcon className='close' close='close' icon={faTimesCircle} onClick={handleClose} />
+
             {createRefferalLinks}
+
+
+            {/* <div className='referral-btn' > */}
+            <Link to="#" className='referral-btn btn' onClick={handleToggle}>
+                {`להתחלת סליקה `}
+                {/* <FontAwesomeIcon className='check' icon={faChevronCircleLeft} /> */}
+                {/* <FontAwesomeIcon className='check' icon={faAngleLeft} /> */}
+            </Link>
+            {/* </div> */}
         </div>
     )
 }
